273e14667b04208824c0512aca05994e1db98b72
mrchopzy-ui <mr.chopzy@gmail.com>
2026-02-19 12:31:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def login():
 
 login()
 
-with open(DATA_FILE, newline="", encoding="latin-1") as f:
+with open(DATA_FILE, "", encoding="latin-1") as f:
     SCRIPS = list(csv.DictReader(f))
 
 def get_watchlists():
